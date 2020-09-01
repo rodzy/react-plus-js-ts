fe454f12fa2b200933ba7b9fb3f87e063a0c3645
rodzy <irod2899@gmail.com>
2020-08-31 21:49:03 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const rules = [
+  {
+    test: /\.tsx?/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    },
+  },
+];
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: { rules },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: "./",
+    port: 4000,
+  },
+};
