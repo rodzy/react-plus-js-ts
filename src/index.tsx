00d389e8c00559815e912e29bccb966c921b9a29
rodzy <irod2899@gmail.com>
2020-08-31 22:04:34 -0600

    Added eslint and updated webpack config

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 export default function App(): JSX.Element {
-  return <h1>Hi,from tsx webpack</h1>;
+    const [count, setcount] = useState(0)
+    return (
+        <>
+        
+        <h1>Hi, from tsx webpack</h1>
+        </>
+  )
 }
 
 const root = document.getElementById("app-root");
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const rules = [
-  {
-    test: /\.tsx?/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
+var path = require("path");
+var rules = [
+    {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+        },
     },
-  },
 ];
 
 module.exports = {
-  target: "web",
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: { rules },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  devServer: {
-    contentBase: "./",
-    port: 4000,
-  },
+    target: "web",
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: { rules: rules },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    devServer: {
+        contentBase: "./",
+        port: 4000,
+    },
 };
